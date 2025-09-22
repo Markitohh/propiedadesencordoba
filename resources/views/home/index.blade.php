e56b2aa7f22edd39b03fc0fbe3b0788829511fcc
Marcos Córdoba <marcoscordobaa.mc@gmail.com>
2025-09-22 13:02:55 -0300

    animaciones al header

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,16 +6,16 @@
                         <div class="md:flex md:justify-between md:items-center md:gap-12">
                             
                             <div class="md:w-3/5 text-center md:text-left mb-8 md:mb-0">
-                                <h2 class="text-5xl leading-tight font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-6xl lg:text-7xl">
+                                <h2 class="text-5xl leading-tight font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-6xl lg:text-7xl animate__animated animate__bounceInLeft">
                                     La propiedad que <span class="text-indigo-600 dark:text-indigo-400">siempre quisiste!</span>
                                 </h2>
-                                <p class="mt-6 max-w-xl mx-auto md:mx-0 text-xl text-gray-600 dark:text-gray-300">
+                                <p class="mt-6 max-w-xl mx-auto md:mx-0 text-xl text-gray-600 dark:text-gray-300 animate__animated animate__bounceInUp">
                                     Encuentra la propiedad de tus sueños en Córdoba. Desde cabañas hasta una casa, ¡y mucho más!
                                 </p>
                                 
                                 <div class="mt-8">
                                     <a href="{{ route('propiedades.create') }}" 
-                                    class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-lg text-white bg-indigo-600 hover:bg-indigo-700 transition duration-300">
+                                    class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-lg text-white bg-indigo-600 hover:bg-indigo-700 transition duration-300 animate__animated animate__fadeIn animate__delay-1s">
                                         Publica tu Propiedad Ahora
                                     </a>
                                 </div>
@@ -25,7 +25,7 @@
                                 <img 
                                     src="{{ asset('img/icon-header.svg') }}" 
                                     alt="Icono del subtitulo"
-                                    class="w-48 h-auto object-cover md:w-full max-w-sm transform hover:scale-105 transition duration-500 ease-in-out"
+                                    class="w-48 h-auto object-cover md:w-full max-w-sm transform hover:scale-105 transition duration-500 ease-in-out animate__animated animate__backInRight"
                                 />
                             </div>
 
